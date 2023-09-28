ae985114c35e75c6b3323a2405dd7283e404840d
circargs <nick@ouellet.dev>
2023-09-27 21:58:43 -0400

    options test

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+"""
+Fixtures for testing.
+"""
+from typing import Iterator
+import pytest
+from keymaker.models import Huggingface
+
+@pytest.fixture
+def distilgpt2() -> Iterator[Huggingface]:
+    """
+    simple model for testing
+    """
+
+    yield Huggingface("distilgpt2")
---
+++ tests/constraints/test_options.py
@@ -0,0 +1,16 @@
+"""Testing the options constraint"""
+
+import pytest
+
+from keymaker import Prompt, CompletionConfig
+
+from keymaker.constraints import OptionsConstraint
+
+
+@pytest.mark.asyncio
+async def test_options_constraint(distilgpt2):
+    prompt = Prompt("An item you would take to the beach: {beach_item}", distilgpt2)
+    beach_items_set = {"Cooler", "Beach Chair", "Sunscreen", "Volleyball", "Suntan Lotion", "Drinks"}
+    beach_items = OptionsConstraint(beach_items_set)
+    filled = await prompt.format(beach_item=CompletionConfig(constraint=beach_items))
+    assert filled.completions['beach_item'] in beach_items_set
