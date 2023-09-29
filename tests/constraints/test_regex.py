e6cc1aba6cb4c5226042675ec2be7006d7e54976
circargs <nick@ouellet.dev>
2023-09-28 20:13:15 -0400

    regex constraint test

---
+++ tests/constraints/test_regex.py
@@ -0,0 +1,15 @@
+"""Testing the options constraint"""
+
+import pytest
+import regex as re
+from keymaker import CompletionConfig, Prompt
+from keymaker.constraints import RegexConstraint
+
+
+@pytest.mark.asyncio
+async def test_options_constraint(distilgpt2):
+    prompt = Prompt("An item you would take to the beach: {beach_item}", distilgpt2)
+    beach_items_set = re.compile(r"Cooler|Beach Chair|Sunscreen|Volleyball|Suntan Lotion|Drinks")
+    beach_items = RegexConstraint(beach_items_set)
+    filled = await prompt.format(beach_item=CompletionConfig(constraint=beach_items))
+    assert filled.completions["beach_item"] in {"Cooler", "Beach Chair", "Sunscreen", "Volleyball", "Suntan Lotion", "Drinks"}
