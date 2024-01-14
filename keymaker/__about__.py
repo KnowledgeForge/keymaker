fb2e1a91c179ff6350cfc17867cd9df5acd37d7a
circargs <nick@ouellet.dev>
2024-01-13 19:46:28 -0500

    fix exception imports

--- keymaker/__about__.py
+++ keymaker/__about__.py
@@ -1,4 +1,4 @@
 """
 Version for Hatch
 """
-__version__ = "0.8.3"
+__version__ = "0.8.4"
--- keymaker/__init__.py
+++ keymaker/__init__.py
@@ -1,2 +1,3 @@
 from .models.base import Model  # noqa: F401
-from .prompt import Completion, CompletionConfig, Prompt, TokenCount, TokenTracker, PromptBudgetException, CompletionBudgetException   # type: ignore # noqa: F401
+from .prompt import Completion, CompletionConfig, Prompt, TokenCount, TokenTracker   # type: ignore # noqa: F401
+from .utils.general import PromptBudgetException, CompletionBudgetException   # type: ignore # noqa: F401
